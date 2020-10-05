0187138d0e46faca4defda38e1436795e68d1b82
shanec7 <56414351+shanec7@users.noreply.github.com>
2020-10-04 18:05:06 -0700

    backspace compare leetcode question solved

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -10,6 +10,9 @@ public class Main {
 	    String t1 = "(({[]}))";
 	    String t2 = "{(})";
         boolean test = validateCharacters(t1);
+        String S = "ab#c";
+        String T = "ad#c";
+        boolean test2 = backspaceCompare(S,T);
     }
 
     public static boolean validateCharacters(String s) {
@@ -33,6 +36,33 @@ public class Main {
         }
 
         return stack.isEmpty();
+    }
 
+    // Runtime: O(N + M) where N is the number of characters in s and M is the number of characters in t.
+    // Space complexity: O(N + M) where N is the number of characters in s and M is the number of characters in t.
+    public static boolean backspaceCompare(String S, String T) {
+        Stack<Character> sStack = new Stack<Character>();
+        for(char c : S.toCharArray()) {
+            if(c != '#') {
+                sStack.push(c);
+            } else if (!sStack.isEmpty()) {
+                sStack.pop();
+            }
+        }
+        Stack<Character> tStack = new Stack<Character>();
+        for(char c : T.toCharArray()) {
+            if(c != '#') {
+                tStack.push(c);
+            } else if (!tStack.isEmpty()) {
+                tStack.pop();
+            }
+        }
+        while(!sStack.isEmpty() && !tStack.isEmpty()) {
+            if(sStack.pop() != tStack.pop()) {
+                return false;
+            }
+        }
+
+        return sStack.isEmpty() && tStack.isEmpty();
     }
 }
