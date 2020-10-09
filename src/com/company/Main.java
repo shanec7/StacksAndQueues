d05c721879a2cd12a73d773cf6052bdaaf8f6bf9
shanec7 <56414351+shanec7@users.noreply.github.com>
2020-10-09 08:49:00 -0700

    greaterElements that uses a stack and a hashmap to map the current value and it's greater value.

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -1,5 +1,7 @@
 package com.company;
 
+import java.util.Arrays;
+import java.util.HashMap;
 import java.util.Queue;
 import java.util.Stack;
 
@@ -14,8 +16,13 @@ public class Main {
         String T = "ad#c";
         //boolean test2 = backspaceCompare(S,T);
         String s = "foobar";
-        String res = removeAdjDupes(s);
-        System.out.println(res);
+        //String res = removeAdjDupes(s);
+        //System.out.println(res);
+        int[] nums1 = {4,1,2};
+        int[] nums2 = {1,3,4,2};
+        int[] result = greaterElements(nums1,nums2);
+        System.out.print(Arrays.toString(result));
+
     }
 
     public static boolean validateCharacters(String s) {
@@ -105,5 +112,32 @@ public class Main {
         }
 
         return result.reverse().toString();
+    }
+
+    public static int[] greaterElements(int[] nums1, int[] nums2) {
+        Stack <Integer> stack = new Stack<>();
+        // hmap containing the current number its looking at and the NEXT number in the array
+        HashMap<Integer, Integer> map = new HashMap<>();
+        for (int i : nums2) {
+            while (!stack.empty() && i > stack.peek()) {
+                map.put(stack.pop(), i);
+            }
+            stack.push(i);
+        }
+
+        for(int i = 0; i < nums1.length; i++) {
+            nums1[i] = map.getOrDefault(nums1[i], -1);
+        }
+
+        return nums1;
+
+        // step example
+        // Input: nums1 = [4,1,2], nums2 = [1,3,4,2]
+        // stack = []
+        // map.put(1, 3)
+        // stack = [3]
+        // map.put(3, 4)
+        // stack = [4]
+
     }
 }
